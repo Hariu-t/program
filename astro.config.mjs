35a1537d5dcfac61256003c24a41154ec89d124d
takumi <t.h.p1217@gmail.com>
2024-11-10 12:58:36 +0900

    t

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Hariu-t.github.io',
-    base: '/program',
+  output: 'server',
+  adapter: netlify(),
 });
